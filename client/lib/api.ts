b2798050b8ad87d4643ed313e2013015bc8f4439
Swas2301 <swastikak231@gmail.com>
2025-06-18 23:25:18 +0530

    deployed

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000/import-logs';
+const API_BASE = `${process.env.BACKEND_URL || 'http://localhost:5000'}/import-logs`;
+
 
 //export const fetchJobs = () => axios.get(`${API_BASE}`);
 export const fetchImportLogs = () => axios.get(`${API_BASE}`);
