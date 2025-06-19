c182856c071a178a75919794edb304f5db4ef59a
Swas2301 <swastikak231@gmail.com>
2025-06-19 16:01:55 +0530

    corrected socket issue

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000';
+const API_BASE = 'https://job-importer.onrender.com';
 
 export const fetchImportLogs = () => axios.get(`${API_BASE}/import-logs`);
 
